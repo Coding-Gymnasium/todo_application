38bdc6f9eab1fe29d88474529cac184d27aa8c34
Nico Rithner <nicorithner@gmail.com>
2021-05-03 09:26:51 -0600

    Render todo items

--- app/javascript/packs/components/TodoApp.js
+++ app/javascript/packs/components/TodoApp.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import TodoItems from './TodoItems';
+import TodoItem from './TodoItem';
 
 class TodoApp extends React.Component {
   constructor(props) {
@@ -22,7 +24,13 @@ class TodoApp extends React.Component {
   }
 
   render() {
-    return <p>TodoApp</p>
+    return (
+      <TodoItems>
+        {this.state.todoItems.map(todoItem => (
+          <TodoItem key={todoItem.id} todoItem={todoItem} />
+        ))}
+      </TodoItems>
+    );
   }
 }
 
